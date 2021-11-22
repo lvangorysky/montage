6240d3afdc1e575293e879daaa9da5a9faae1f45
yhh <yaohonghui@zhuangdianwenhua.com>
2021-11-22 09:44:24 +0800

    update: new

---
+++ leecode/324.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ * 给你一个整数数组 nums，将它重新排列成 nums[0] < nums[1] > nums[2] < nums[3]... 的顺序。
+ * 数组摆动问题
+ */
+ var wiggleSort = function(nums) {
+    // return nums
+    for (let i=0;i<nums.length;i++) {
+        nums[i]
+    }
+};
+
+wiggleSort([1,2,3,4,5,6])
